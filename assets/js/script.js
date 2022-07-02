e314b091ca723c7017d2f5a8d21394d81313682d
Cloud <m.melanson000@gmail.com>
2022-07-02 10:46:17 -0400

    color coding and time function related to colors done

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,41 @@
+var presentHour = moment().hour();
+var inputForms = document.getElementsByTagName("input");
+
 window.setInterval(function () {
-    $('#currentDay').html(moment().format("dddd, MMMM Do YYYY, h:mm:ss a"))
+    if (presentHour !== moment().hour()) {
+        renderBackgrounds();
+    }
+    $('#currentDay').html(moment().format("dddd, MMMM Do YYYY, h:mm:ss a"));
+    presentHour = moment().hour();
 }, 1000);
+
+
+
+
+function renderBackgrounds() {
+    for (var i = 0; i < inputForms.length; i++) {
+        if (parseInt(inputForms[i].id) < presentHour) {
+            inputForms[i].classList.add('past');
+        } else if (parseInt(inputForms[i].id) === presentHour) {
+            inputForms[i].classList.add('present');
+        } else {
+            inputForms[i].classList.add('future');
+        }
+    }
+}
+
+//     }
+//         console.log(this.id);
+//         if (this.id < presentHour) {
+//             this.classList.add("past");
+//             console.log ("step 1");
+//         } else if (this.id === presentHour) {
+//             this.classList.add("present");
+//             console.log ("step 2");
+//         } else {
+//             this.classList.add("future");
+//             console.log ("step 3");
+//         }
+
+
+renderBackgrounds();
